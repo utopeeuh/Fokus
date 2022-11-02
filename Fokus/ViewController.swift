379a729906379c399225e7f28e88aea6223ef898
Uto <daffamadeo@gmail.com>
2022-11-02 20:28:22 +0700

    Initial Commit

---
+++ Fokus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fokus
+//
+//  Created by Tb. Daffa Amadeo Zhafrana on 02/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
