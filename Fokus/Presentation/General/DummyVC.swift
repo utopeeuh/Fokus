a2634dcbfdf0b3d9ba4378d2a5ad8c36bf8dad13
Uto <daffamadeo@gmail.com>
2022-11-16 20:55:19 +0700

    Inital folder structure

---
+++ Fokus/Presentation/General/DummyVC.swift
@@ -0,0 +1,29 @@
+//
+//  DummyVC.swift
+//  Fokus
+//
+//  Created by Tb. Daffa Amadeo Zhafrana on 16/11/22.
+//
+
+import UIKit
+
+class DummyVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
