90099255455c8c27dc83849a416254a32ebb1c04
Firzha Ardhia Ramadhan <firzharamadhan27@gmail.com>
2022-11-20 01:06:39 +0700

    fix conflict part three

--- Fokus/Presentation/General/DummyVC.swift
+++ Fokus/Presentation/General/DummyVC.swift
@@ -9,24 +9,78 @@ import UIKit
 import SnapKit
 
 class DummyVC: UIViewController {
+    
+    let data = ["hahaha", "hiihih", "diididid"]
+    
+    let button: UIButton = {
         
-    let profileTest = ProfileView()
+        let container = UIButton()
+        
+        container.backgroundColor = .red
+        container.layer.borderColor = UIColor.white.cgColor
+        container.layer.borderWidth = 1
+        container.setTitleColor(.white, for: .selected)
+        
+        
+        container.setTitle("Not selected", for: .normal)
+        container.setTitle("hello world!", for: .selected)
+        
+        
+        container.isUserInteractionEnabled = true
+        
+        
+        return container
+    }()
+    
+    private let homeFeedTable: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(TaskListCell.self, forCellReuseIdentifier: "TaskListCell")
+        tableView.isScrollEnabled = true
+        tableView.backgroundColor = .clear
+        tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
+        return tableView
+    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.isNavigationBarHidden = true
+        view.addSubview(button)
         
         view.backgroundColor = .systemBackground
         view.addSubview(profileTest)
         
-        profileTest.snp.makeConstraints { make in
-            make.left.equalToSuperview().offset(20)
-            make.right.equalToSuperview().offset(-60)
-            make.height.equalTo(150)
-            make.top.equalTo(view.safeAreaLayoutGuide)
-
-//            make.top.equalTo(view.safeAreaLayoutGuide)
-//            make.bottom.equalTo(view.safeAreaLayoutGuide)
+//        view.addSubview(homeFeedTable)
+//
+        homeFeedTable.delegate = self
+        homeFeedTable.dataSource = self
+        homeFeedTable.reloadData()
+        
+        view.addSubview(homeFeedTable)
+        
+        homeFeedTable.snp.makeConstraints { make in
+            make.width.equalToSuperview().offset(-40)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(data.count*100)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
         }
+        
+        button.snp.makeConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(300)
+        }
+       
+
+    }
+    
+}
+
+extension DummyVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
